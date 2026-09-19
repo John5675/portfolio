51e56eb57ae982eb09f747cba509fe9c30f09206
jhuynh <johnhuynh238@gmail.com>
2026-09-19 01:17:46 -0700

    Clear project to start from scratch

--- src/components/Icon.jsx
+++
@@ -1,7 +0,0 @@
-export default function Icon({ name, className = 'ico', style }) {
-  return (
-    <svg className={className} style={style} aria-hidden="true">
-      <use href={`#i-${name}`} />
-    </svg>
-  );
-}
